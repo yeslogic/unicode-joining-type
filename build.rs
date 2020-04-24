b9f712326669df2086e3ce8193c27aa6870918e4
Wesley Moore <wes@wezm.net>
2020-04-24 15:30:29 +1000

    Make the build script generic

--- build.rs
+++ build.rs
@@ -8,67 +8,73 @@ use std::fs::File;
 use std::io::Write;
 use std::path::{Path, PathBuf};
 
-use block::{Block, LAST_INDEX};
+use block::{Block, BlockSize};
 use tables::{JoiningType, JOINING_TYPE};
-
-const SHIFT: u32 = block::LAST_INDEX.count_ones();
 
 fn main() {
     let output_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("joining_type.rs");
+    let joining_type_table = CompiledTable::compile(JOINING_TYPE);
 
-    write_table(&output_path, &compile_table());
+    write_joining_type_table(&output_path, &joining_type_table);
 }
 
-struct CompiledTable {
-    blocks: Vec<(u32, Block)>,
+struct CompiledTable<T>
+where
+    T: Default + BlockSize + Copy,
+{
+    blocks: Vec<(u32, Block<T>)>,
     address_to_block_index: Vec<(u32, usize)>,
     last_code_point: u32,
 }
 
-fn compile_table() -> CompiledTable {
-    let mut blocks = Vec::new();
-    let mut address_to_block_index = Vec::new();
+impl<T> CompiledTable<T>
+where
+    T: Default + BlockSize + Copy + Eq,
+{
+    fn compile(table: &[(u32, u32, T)]) -> Self {
+        let last_index = T::last_index();
+        let shift = last_index.count_ones();
+        let mut blocks = Vec::new();
+        let mut address_to_block_index = Vec::new();
 
-    let &(start, _, _) = JOINING_TYPE
-        .iter()
-        .min_by_key(|(start, _, _)| start)
-        .unwrap();
-    let &(_, end, _) = JOINING_TYPE.iter().max_by_key(|(_, end, _)| end).unwrap();
-    let last_code_point = end;
+        let &(start, _, _) = table.iter().min_by_key(|(start, _, _)| start).unwrap();
+        let &(_, end, _) = table.iter().max_by_key(|(_, end, _)| end).unwrap();
+        let last_code_point = end;
 
-    // Extend end to the end of the last block to ensure the last block is written out
-    let end_block_address = end & (!LAST_INDEX as u32);
-    let end = end_block_address + block::SIZE as u32;
+        // Extend end to the end of the last block to ensure the last block is written out
+        let end_block_address = end & (!last_index as u32);
+        let end = end_block_address + T::SIZE as u32;
 
-    let mut block = Block::new();
-    for codepoint in start..=end {
-        let joining_type = lookup(codepoint);
-        let block_address = (codepoint >> SHIFT).saturating_sub(1) << SHIFT;
+        let mut block = Block::new();
+        for codepoint in start..=end {
+            let joining_type = lookup(codepoint, table);
+            let block_address = (codepoint >> shift).saturating_sub(1) << shift;
 
-        // This is the first codepoint in this block, write out the previous block
-        if codepoint != 0 && (codepoint & u32::try_from(block::LAST_INDEX).unwrap()) == 0 {
-            if let Some(index) = blocks.iter().position(|(_, candidate)| candidate == &block) {
-                address_to_block_index.push((block_address, index));
-            } else {
-                // Add the block if it's new
-                address_to_block_index.push((block_address, blocks.len()));
-                blocks.push((block_address, block.clone()));
+            // This is the first codepoint in this block, write out the previous block
+            if codepoint != 0 && (codepoint & u32::try_from(last_index).unwrap()) == 0 {
+                if let Some(index) = blocks.iter().position(|(_, candidate)| candidate == &block) {
+                    address_to_block_index.push((block_address, index));
+                } else {
+                    // Add the block if it's new
+                    address_to_block_index.push((block_address, blocks.len()));
+                    blocks.push((block_address, block.clone()));
+                }
+
+                block.reset();
             }
 
-            block.reset();
+            block[usize::try_from(codepoint).unwrap() & last_index] = joining_type;
         }
 
-        block[usize::try_from(codepoint).unwrap() & block::LAST_INDEX] = joining_type;
-    }
-
-    CompiledTable {
-        blocks,
-        address_to_block_index,
-        last_code_point,
+        CompiledTable {
+            blocks,
+            address_to_block_index,
+            last_code_point,
+        }
     }
 }
 
-fn write_table(path: &Path, compiled_table: &CompiledTable) {
+fn write_joining_type_table(path: &Path, compiled_table: &CompiledTable<JoiningType>) {
     let mut output =
         File::create(&path).expect(&format!("unable to open {}", path.to_string_lossy()));
 
@@ -81,13 +87,13 @@ fn write_table(path: &Path, compiled_table: &CompiledTable) {
         compiled_table.last_code_point
     )
     .unwrap();
-    writeln!(output, "\nconst BLOCK_SIZE: usize = {};", block::SIZE).unwrap();
+    writeln!(output, "\nconst BLOCK_SIZE: usize = {};", JoiningType::SIZE).unwrap();
 
     // Write out the blocks in address order
     writeln!(
         output,
         "\nconst JOINING_TYPE_BLOCKS: [JoiningType; {}] = [",
-        compiled_table.blocks.len() * block::SIZE
+        compiled_table.blocks.len() * JoiningType::SIZE
     )
     .unwrap();
 
@@ -113,7 +119,7 @@ fn write_table(path: &Path, compiled_table: &CompiledTable) {
             output,
             "const BLOCK_OFFSET_{:04X}: u16 = 0x{:04X};",
             address,
-            index * block::SIZE
+            index * JoiningType::SIZE
         )
         .unwrap();
     }
@@ -132,9 +138,12 @@ fn write_table(path: &Path, compiled_table: &CompiledTable) {
     writeln!(output, "];").unwrap();
 }
 
-/// Lookup this code point in the JOINING_TYPE table
-fn lookup(codepoint: u32) -> JoiningType {
-    JOINING_TYPE
+/// Lookup this code point in `table`
+fn lookup<T>(codepoint: u32, table: &[(u32, u32, T)]) -> T
+where
+    T: Default + BlockSize + Eq + Copy,
+{
+    table
         .binary_search_by(|&(start, end, _)| {
             if codepoint < start {
                 Ordering::Greater
@@ -145,52 +154,76 @@ fn lookup(codepoint: u32) -> JoiningType {
             }
         })
         .ok()
-        .map(|idx| JOINING_TYPE[idx].2)
-        .unwrap_or(JoiningType::NonJoining)
+        .map(|idx| table[idx].2)
+        .unwrap_or_default()
+}
+
+impl Default for JoiningType {
+    fn default() -> Self {
+        JoiningType::NonJoining
+    }
+}
+
+impl BlockSize for JoiningType {
+    const SIZE: usize = 256;
 }
 
 mod block {
-    pub const SIZE: usize = 256;
-    pub const LAST_INDEX: usize = SIZE - 1;
-
-    use super::JoiningType;
     use std::ops::{Index, IndexMut};
+
+    pub trait BlockSize {
+        const SIZE: usize;
+
+        fn last_index() -> usize {
+            Self::SIZE - 1
+        }
+    }
 
     /// A fixed size block
     ///
     /// Ideally this would be an array but that doesn't work until const generics are stabilised
     #[derive(Debug, PartialEq, Eq, Hash, Clone)]
-    pub struct Block {
-        data: Vec<JoiningType>,
+    pub struct Block<T>
+    where
+        T: Default + BlockSize + Copy,
+    {
+        data: Vec<T>,
     }
 
-    impl Block {
+    impl<T> Block<T>
+    where
+        T: Default + BlockSize + Copy,
+    {
         pub fn new() -> Self {
             Block {
-                data: vec![JoiningType::NonJoining; SIZE],
+                data: vec![T::default(); T::SIZE],
             }
         }
 
         pub fn reset(&mut self) {
-            self.data
-                .iter_mut()
-                .for_each(|val| *val = JoiningType::NonJoining);
+            self.data.iter_mut().for_each(|val| *val = T::default());
         }
 
-        pub fn iter(&self) -> impl Iterator<Item = &JoiningType> {
+        pub fn iter(&self) -> impl Iterator<Item = &T> {
             self.data.iter()
         }
     }
 
-    impl Index<usize> for Block {
-        type Output = JoiningType;
+    impl<T> Index<usize> for Block<T>
+    where
+        T: Default + BlockSize + Copy,
+    {
+        type Output = T;
 
         fn index(&self, index: usize) -> &Self::Output {
             &self.data[index]
         }
     }
 
-    impl IndexMut<usize> for Block {
+    impl<T> IndexMut<usize> for Block<T>
+    where
+        T: Default + BlockSize + Copy,
+    {
         fn index_mut(&mut self, index: usize) -> &mut Self::Output {
             self.data.index_mut(index)
         }
